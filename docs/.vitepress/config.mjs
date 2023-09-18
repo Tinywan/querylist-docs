5337b3d6eda5edec3dd9f7054133d5c649bbf6c4
Jaeger <JaegerCode@gmail.com>
2023-09-19 07:21:50 +0800

    nav

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,21 +8,44 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
+      { text: '入门指南', link: '/markdown-examples' },
+      { text: 'API手册', link: '/markdown-examples' },
       {
-        text: 'Examples',
+        text: '版本',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'v4', link: '/v4/guide/', activeMatch: '/v4/' },
+          { text: 'v3', link: '/v3/guide/', activeMatch: '/v3/' }
         ]
       }
     ],
 
+    sidebar: {
+      '/v4/guide/': [{
+        text: 'Examples V4',
+        items: [
+          { text: 'overview V4', link: '/v4/guide/overview' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }],
+      '/v3/guide/': [{
+        text: 'Examples V3',
+        items: [
+          { text: 'Markdown Examples V3', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }]
+    },
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/jae-jae/querylist-docs/edit/main/docs/:path'
+    },
+    search: {
+      provider: 'local'
+    }
+  },
+  lastUpdated: true
 })
